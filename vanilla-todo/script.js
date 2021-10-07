20f9f295eaaf44fb0446e64f25aad4b8b7f7b78e
Cedrick Garcia <icedgarcia@gmail.com>
2021-10-07 22:13:59 +0800

    substitute for insertAdjacentHTML

--- vanilla-todo/script.js
+++ vanilla-todo/script.js
@@ -3,7 +3,7 @@ const todoInput = document.querySelector('.todo-input')
 const todoList = document.querySelector('.todo-list')
 
 
-
+// 1 submit todo/ forms
 const handleSubmit = (e)=> {
   e.preventDefault();
   const todoValue = todoInput.value.trim();
@@ -12,9 +12,34 @@ const handleSubmit = (e)=> {
   if(todoValue !== '' ) { //to enable empty todo
     todoInput.value = ''
     console.log(todoValue)
+    addTodo(todoValue)
   }
-
 }
+
+
+
+const addTodo = (todoValue) => {
+  const li = document.createElement('li')
+
+
+  li.innerHTML = `
+  <span>${todoValue}</span>
+  
+  `
+  console.log(li)
+  // const position = 'beforeend'
+  
+  todoList.append(li)
+  todoList.insertBefore(li, todoList.childNodes[0])
+
+  // const list = 'list';
+  // list.insertBefore(todoList, list.children)
+}
+
+
+
+
+
 
 // todoForm.addEventListener()
 document.addEventListener('submit', handleSubmit)
